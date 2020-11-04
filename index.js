a53db097c1fdbbb0ab126c8eb5d88495860f6bb7
Shawn Rushefsky <shawn.rushefsky@gmail.com>
2020-11-04 13:49:50 -0600

    derp

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ async function run() {
     const token = core.getInput('token', {required: true});
     const octokit = github.getOctokit(token);
 
-    const payload = JSON.stringify(github.context.payload, undefined, 2);
-    console.log(payload.pull_request);
+    const payload = github.context.payload;
+    console.log(payload.pull_request._links.commits.href);
   } catch (error) {
     core.setFailed(error.message);
   }
