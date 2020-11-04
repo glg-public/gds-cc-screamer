2854999f25a49fa7c1eaa15c2f1c574d22789b01
Shawn Rushefsky <shawn.rushefsky@gmail.com>
2020-11-04 13:36:35 -0600

    get commits link, auth octokit

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(payload.pull_request._links.commits.href);
-} catch (e) {
-  core.setFailed(error.message);
+async function run() {
+  try {
+    const token = core.getInput('token');
+    const octokit = github.getOctokit(token);
+
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    console.log(payload.pull_request._links.commits.href);
+  } catch (e) {
+    core.setFailed(error.message);
+  }
 }
+
+run();
