dec90260998703a75fdfc0899b7c0d12bca6c968
Shawn Rushefsky <shawn.rushefsky@gmail.com>
2020-11-04 13:45:07 -0600

    flag as required

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const github = require('@actions/github');
 
 async function run() {
   try {
-    const token = core.getInput('token');
+    const token = core.getInput('token', {required: true});
     const octokit = github.getOctokit(token);
 
     const payload = JSON.stringify(github.context.payload, undefined, 2);
