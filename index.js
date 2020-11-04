f516f8e96ec81fab4ea2588cc3871eb741d32194
Shawn Rushefsky <shawn.rushefsky@gmail.com>
2020-11-04 14:10:45 -0600

    get files

--- index.js
+++ index.js
@@ -6,8 +6,17 @@ async function run() {
     const token = core.getInput('token', {required: true});
     const octokit = github.getOctokit(token);
 
-    const payload = github.context.payload;
-    console.log(JSON.stringify(payload.pull_request, undefined, 2));
+    const pr = github.context.payload.pull_request;
+    const owner = pr.base.repo.owner.login;
+    const repo = pr.base.repo.name;
+    const pull_number = pr.number;
+    const files = await octokit.pulls.listFiles({
+      owner,
+      repo,
+      pull_number
+    });
+    console.log(owner, repo, pull_number);
+    console.log(JSON.stringify(files, undefined, 2));
   } catch (error) {
     core.setFailed(error.message);
   }
