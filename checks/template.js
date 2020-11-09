7fb2af2418342ede09d32143855b3e2cd53556d7
Shawn Rushefsky <shawn.rushefsky@gmail.com>
2020-11-09 15:04:32 -0600

    update template check

--- checks/template.js
+++ checks/template.js
@@ -7,15 +7,24 @@ const core = require('@actions/core');
  */
 async function templateCheck(orders, context) {
   core.info(`Template Check - ${orders.path}`);
-  const problems = [];
-  let lineNumber = 0;
-
-  return [{
+  const results = []
+  /**
+   * A Result Object:
+   {
     title: 'Failing Check',
-    problems,
+    problems: ['This code sucks'],
     line: lineNumber,
     level: 'failure' // must be "failure", "warning", "notice", or "success"
-  }];
+   }
+   */
+  
+  for (let i = 0; i < orders.contents.length; i++) {
+    const line = orders.contents[i];
+    const lineNumber = i+1;
+    // Do something
+  }
+
+  return results;
 }
 
 module.exports = templateCheck;
