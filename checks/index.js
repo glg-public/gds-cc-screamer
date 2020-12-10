04f563956ac23373be1b6ac437b5f9823f769b80
Shawn Rushefsky <shawn.rushefsky@gmail.com>
2020-12-10 15:10:56 -0600

    add to checks

--- checks/index.js
+++ checks/index.js
@@ -13,5 +13,6 @@ module.exports = [
   require('./no-sourcing'),
   require('./jwt-access-flags'),
   require('./no-spaces-in-exports'),
-  require('./secrets-json-valid')
+  require('./secrets-json-valid'),
+  require('./policy-json-valid')
 ];
