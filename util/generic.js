b092796017a3df06a02f9f46fb418896bb3e305b
Shawn Rushefsky <shawn.rushefsky@gmail.com>
2021-01-22 14:58:19 -0600

    refactor util

---
+++ util/generic.js
@@ -0,0 +1,61 @@
+require("../typedefs");
+const path = require("path");
+const { camelCaseFileName } = require("./text");
+const fs = require("fs").promises;
+
+const jobdeploy = /^jobdeploy (?<source>\w+)\/(?<org>[\w-]+)\/(?<repo>.+?)\/(?<branch>.+?):(?<tag>\w+)/;
+
+/**
+ *
+ * @param {Array<string>} fileLines
+ *
+ * @returns {boolean}
+ */
+function isAJob(fileLines) {
+  const isJobDeploy =
+    fileLines.filter((line) => jobdeploy.test(line)).length > 0;
+  const isUnpublished =
+    fileLines.filter((line) => line === "unpublished").length > 0;
+
+  return isJobDeploy || isUnpublished;
+}
+
+
+/**
+ * Read orders, secrets.json, and policy.json from the directory,
+ * and split them by \n.
+ * @param {String} filePath the path for the orders file
+ * @returns {Deployment}
+ */
+async function getContents(serviceName, filesToCheck) {
+  const result = { serviceName };
+  for (let filename of filesToCheck) {
+    const filepath = path.join(serviceName, filename);
+    try {
+      await fs.stat(filepath);
+      const contents = await fs.readFile(filepath, "utf8");
+      result[`${camelCaseFileName(filename)}Path`] = filepath;
+      result[`${camelCaseFileName(filename)}Contents`] = contents.split("\n");
+    } catch (e) {
+      // No particular file is required in order to run the check suite
+    }
+  }
+  return result;
+}
+
+
+function getExportValue(text, varName) {
+  const regex = new RegExp(`^export ${varName}=(.*)`, "mi");
+  const match = regex.exec(text);
+
+  if (!match || match.length < 2 || match[1].length < 1) return null;
+
+  const value = match[1].replace(/['|"]/gm, "");
+  return value && value.length > 0 ? value : null;
+}
+
+module.exports = {
+  isAJob,
+  getContents,
+  getExportValue
+}
---
+++ util/index.js
@@ -0,0 +1,51 @@
+const {
+  codeBlock,
+  suggest,
+  getLinesForJSON,
+  getLineNumber,
+  getLineWithinObject,
+  escapeRegExp,
+  detectIndentation,
+  camelCaseFileName,
+} = require("./text");
+
+const {
+  getAllDeployments,
+  leaveComment,
+  suggestBugReport,
+  lineLink,
+  prLink,
+  getNewIssueLink,
+  getNewFileLink,
+  getOwnerRepoBranch,
+  clearPreviousRunComments,
+} = require("./github");
+
+const {
+  isAJob,
+  getContents,
+  getExportValue
+} = require("./generic");
+
+module.exports = {
+  getLinesForJSON,
+  suggest,
+  getLineNumber,
+  getLineWithinObject,
+  getNewFileLink,
+  getOwnerRepoBranch,
+  isAJob,
+  escapeRegExp,
+  detectIndentation,
+  leaveComment,
+  suggestBugReport,
+  getAllDeployments,
+  clearPreviousRunComments,
+  getContents,
+  camelCaseFileName,
+  getExportValue,
+  getNewIssueLink,
+  codeBlock,
+  prLink,
+  lineLink,
+};
