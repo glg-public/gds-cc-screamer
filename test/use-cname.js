0c55813416eead9b3db026df2446aec1ae82becd
Shawn Rushefsky <shawn.rushefsky@gmail.com>
2021-01-25 14:53:42 -0600

    check for cluster dns use

---
+++ test/use-cname.js
@@ -0,0 +1,26 @@
+const { expect } = require("chai");
+const useCNAME = require("../checks/use-cname");
+
+describe("Use CNAME instead of cluster dns", () => {
+  it("warns when it encounters a url that looks like a cluster dns", async () => {
+    const deployment = {
+      serviceName: "streamliner",
+      ordersPath: "streamliner/orders",
+      ordersContents: [
+        "export PUBLIC_URL=https://s99.glgresearch.com/streamliner",
+      ],
+    };
+
+    const results = await useCNAME(deployment);
+    expect(results.length).to.equal(1);
+    expect(results[0]).to.deep.equal({
+      title: "Use friendly CNAME instead",
+      line: 1,
+      level: "warning",
+      path: deployment.ordersPath,
+      problems: [
+        "Rather than using the cluster dns (`s99.glgresearch.com`), consider using the friendly CNAME (e.g. `streamliner.glgresearch.com`)",
+      ],
+    });
+  });
+});
