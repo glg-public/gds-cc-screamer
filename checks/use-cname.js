7f65f60a5ada35667fa892c19c3147526e61a481
Shawn Rushefsky <shawn.rushefsky@gmail.com>
2021-01-25 15:00:59 -0600

    remove unused things

--- checks/use-cname.js
+++ checks/use-cname.js
@@ -7,12 +7,10 @@ const clusterDNS = /^https:\/\/(?<clusterId>[spji]\d\d)\.glgresearch\.com/;
 /**
  * Accepts a deployment object, and does some kind of check
  * @param {Deployment} deployment An object containing information about a deployment
- * @param {GitHubContext} context The context object provided by github
- * @param {ActionInputs} inputs The inputs (excluding the token) from the github action
  *
  * @returns {Array<Result>}
  */
-async function useCNAME(deployment, context, inputs) {
+async function useCNAME(deployment) {
   /**
    * You should check the existance of any file you're trying to check
    */
@@ -22,17 +20,7 @@ async function useCNAME(deployment, context, inputs) {
   }
   core.info(`Use CNAME instead of cluster DNS - ${deployment.ordersPath}`);
   const results = [];
-  /**
-   * A Result Object:
-   {
-    title: 'Failing Check',
-    problems: ['This code sucks'],
-    line: lineNumber,
-    level: 'failure' // must be "failure", "warning", "notice", or "success"
-    [path]: deployment.secretsJsonPath // This defaults to the deployment path, but you can override for different files.
-   }
-   */
-
+  
   deployment.ordersContents.forEach((line, i) => {
     const lineNumber = i + 1;
     const envvarMatch = envvar.exec(line);
