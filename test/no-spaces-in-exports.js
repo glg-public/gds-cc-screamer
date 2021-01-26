0d46e175eacc86bb7e036ff9c7709e87e488b153
Shawn Rushefsky <shawn.rushefsky@gmail.com>
2021-01-26 14:22:41 -0600

    100% for no-spaces-in-exports

--- test/no-spaces-in-exports.js
+++ test/no-spaces-in-exports.js
@@ -2,6 +2,15 @@ const { expect } = require("chai");
 const noSpaces = require("../checks/no-spaces-in-exports");
 
 describe("No Spaces in Exports Check", () => {
+  it("skips if there is no orders file", async () => {
+    const deployment = {
+      serviceName: "streamliner"
+    };
+
+    const results = await noSpaces(deployment);
+    expect(results.length).to.equal(0);
+  });
+
   it("allows orders where all exports have proper spacing", async () => {
     const deployment = {
       serviceName: "streamliner",
@@ -10,6 +19,7 @@ describe("No Spaces in Exports Check", () => {
         "export HEALTHCHECK=/diagnostic",
         "export SECURITY_MODE=jwt",
         'export CAT="pants"',
+        "dockerdeploy github/glg/streamliner/main:latest"
       ],
     };
 
