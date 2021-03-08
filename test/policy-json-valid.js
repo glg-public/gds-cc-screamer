5dadac867254b46c03246ed02cf498610f92d7a7
Shawn Rushefsky <shawn.rushefsky@gmail.com>
2021-03-08 14:30:12 -0600

    fix test

--- test/policy-json-valid.js
+++ test/policy-json-valid.js
@@ -720,7 +720,7 @@ describe("policy.json is valid", () => {
           "```suggestion\n" +
           "    },\n" +
           "    {\n" +
-          '      "Sid": "AllowRequiredSecrets",\n' +
+          '      "Sid": "AllowSecretsAccess",\n' +
           '      "Effect": "Allow",\n' +
           '      "Action": "secretsmanager:GetSecretValue",\n' +
           '      "Resource": [\n' +
