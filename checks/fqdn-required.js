67af6509081bf924823760962a6b03142fd246aa
Shawn Rushefsky <shawn.rushefsky@gmail.com>
2021-03-11 11:51:38 -0600

    new check for fqdn

---
+++ checks/fqdn-required.js
@@ -0,0 +1,117 @@
+require("../typedefs");
+const core = require("@actions/core");
+const { isAJob, suggest } = require("../util");
+
+const envvar = /^(export |)(\w+)=['"]?(.+?)['"]?$/;
+
+/**
+ * Accepts a deployment object, and does some kind of check
+ * @param {Deployment} deployment An object containing information about a deployment
+ * @param {GitHubContext} context The context object provided by github
+ * @param {ActionInputs} inputs The inputs (excluding the token) from the github action
+ *
+ * @returns {Array<Result>}
+ */
+async function fqdnRequired(deployment, context, inputs, httpGet) {
+  /**
+   * You should check the existance of any file you're trying to check
+   */
+  if (!deployment.ordersContents) {
+    console.log(`No Orders Present - Skipping ${deployment.serviceName}`);
+    return [];
+  }
+
+  if (isAJob(deployment.ordersContents)) {
+    console.log(`Jobs don't need domains - Skipping ${deployment.serviceName}`);
+    return [];
+  }
+  console.log(`GDS_FQDN is set - ${deployment.ordersPath}`);
+
+  /** @type {Array<Result>} */
+  const results = [];
+
+  const repo = context.payload.pull_request.base.repo.name;
+  const splitName = repo.split(".");
+  const repoCluster = splitName.pop();
+  const clusterMap = await httpGet(inputs.clusterMap);
+
+  let myCluster = { hosts: [] };
+  if (clusterMap[repoCluster]) {
+    myCluster = clusterMap[repoCluster];
+  }
+  if (!myCluster.hosts) {
+    myCluster.hosts = [];
+  }
+
+  let isSet = false;
+  let lastEnvLine = 0;
+  deployment.ordersContents.forEach((line, i) => {
+    const lineNumber = i + 1;
+    const match = envvar.exec(line);
+    if (match) {
+      const [, , variable, value] = match;
+      lastEnvLine = lineNumber;
+      if (variable === "GDS_FQDN") {
+        isSet = true;
+        if (myCluster.hosts.length > 0 && !myCluster.hosts.includes(value)) {
+          const result = {
+            title: "You Must Specify A Valid Domain Name For This Cluster",
+            level: "failure",
+            problems: [],
+            path: deployment.ordersPath,
+            line: lineNumber,
+          };
+          result.problems.push(
+            `Like one of the following:\n${myCluster.hosts
+              .map((host) => suggest("", line.replace(value, host)))
+              .join("\n")}`
+          );
+          results.push(result);
+        } else if (myCluster.hosts.length === 0) {
+          results.push({
+            title: "This Cluster Does Not Have Any Associated Hostnames",
+            level: "warning",
+            problems: [
+              "It is likely that your service will not be reachable at this address, as there are currently no domains associated with this cluster.",
+            ],
+            path: deployment.ordersPath,
+            line: lineNumber,
+          });
+        }
+      }
+    }
+  });
+
+  if (!isSet) {
+    const result = {
+      title: "You Must Specify a Preferred Domain Name",
+      level: "failure",
+      problems: [],
+      path: deployment.ordersPath,
+      line: lastEnvLine + 1,
+    };
+
+    if (myCluster.hosts.length === 0) {
+      result.problems.push("(e.g. `streamliner.glgresearch.com`)");
+    } else {
+      result.problems.push(
+        `Like one of the following:\n${myCluster.hosts
+          .map((host) =>
+            suggest(
+              "",
+              `${
+                deployment.ordersContents[result.line] || ""
+              }\n# Preferred Fully Qualified Domain Name\nexport GDS_FQDN='${host}'\n`
+            )
+          )
+          .join("\n")}`
+      );
+    }
+
+    results.push(result);
+  }
+
+  return results;
+}
+
+module.exports = fqdnRequired;
---
+++ test/fqdn-required.js
@@ -0,0 +1,162 @@
+const { expect } = require("chai");
+const fs = require("fs").promises;
+const fqdnRequired = require("../checks/fqdn-required");
+
+// This makes unit testing much simpler
+async function localGet(path) {
+  const content = await fs.readFile(path, "utf8");
+  return JSON.parse(content);
+}
+
+const inputs = {
+  clusterMap: "test/fixtures/cluster-map.json",
+};
+
+describe("FQDN is Required", () => {
+  it("accepts orders with a fqdn that is associated with the cluster", async () => {
+    const deployment = {
+      serviceName: "streamliner",
+      ordersPath: "streamliner/orders",
+      ordersContents: ["export GDS_FQDN='streamliner.glgresearch.com'"],
+    };
+
+    const context = {
+      payload: {
+        pull_request: {
+          base: {
+            repo: {
+              name: "gds.clusterconfig.s99",
+            },
+          },
+        },
+      },
+    };
+
+    const results = await fqdnRequired(deployment, context, inputs, localGet);
+
+    expect(results.length).to.equal(0);
+  });
+
+  it("skips if there are no orders present", async () => {
+    const deployment = {
+      serviceName: "streamliner",
+    };
+
+    const context = {
+      payload: {
+        pull_request: {
+          base: {
+            repo: {
+              name: "gds.clusterconfig.s99",
+            },
+          },
+        },
+      },
+    };
+
+    const results = await fqdnRequired(deployment, context, inputs, localGet);
+
+    expect(results.length).to.equal(0);
+  });
+
+  it("skips if the deployment is a job", async () => {
+    const deployment = {
+      serviceName: "streamliner",
+      ordersPath: "streamliner/orders",
+      ordersContents: ["unpublished"],
+    };
+
+    const context = {
+      payload: {
+        pull_request: {
+          base: {
+            repo: {
+              name: "gds.clusterconfig.s99",
+            },
+          },
+        },
+      },
+    };
+
+    const results = await fqdnRequired(deployment, context, inputs, localGet);
+
+    expect(results.length).to.equal(0);
+  });
+
+  it("warns if domain name is specified, but none are listed for the cluster", async () => {
+    const deployment = {
+      serviceName: "streamliner",
+      ordersPath: "streamliner/orders",
+      ordersContents: ["export GDS_FQDN='streamliner.glgresearch.com'"],
+    };
+
+    const context = {
+      payload: {
+        pull_request: {
+          base: {
+            repo: {
+              name: "gds.clusterconfig.s98",
+            },
+          },
+        },
+      },
+    };
+
+    const results = await fqdnRequired(deployment, context, inputs, localGet);
+
+    expect(results.length).to.equal(1);
+    expect(results[0].level).to.equal("warning");
+  });
+
+  it("fails if a domain name is not defined", async () => {
+    const deployment = {
+      serviceName: "streamliner",
+      ordersPath: "streamliner/orders",
+      ordersContents: ["export HEALTHCHECK='/healthcheck'"],
+    };
+
+    const context = {
+      payload: {
+        pull_request: {
+          base: {
+            repo: {
+              name: "gds.clusterconfig.s99",
+            },
+          },
+        },
+      },
+    };
+
+    const results = await fqdnRequired(deployment, context, inputs, localGet);
+
+    expect(results.length).to.equal(1);
+    expect(results[0].level).to.equal("failure");
+    expect(results[0].line).to.equal(2); // proposes adding after the rest of the environment
+  });
+
+  it("fails if a domain name is defined that is not associated with the cluster", async () => {
+    const deployment = {
+      serviceName: "streamliner",
+      ordersPath: "streamliner/orders",
+      ordersContents: ["export GDS_FQDN='wrong.glgresearch.com'"],
+    };
+
+    const context = {
+      payload: {
+        pull_request: {
+          base: {
+            repo: {
+              name: "gds.clusterconfig.s99",
+            },
+          },
+        },
+      },
+    };
+
+    const results = await fqdnRequired(deployment, context, inputs, localGet);
+
+    expect(results.length).to.equal(1);
+    expect(results[0].level).to.equal("failure");
+    expect(results[0].line).to.equal(1); // proposes replacing the offending line
+  });
+});
