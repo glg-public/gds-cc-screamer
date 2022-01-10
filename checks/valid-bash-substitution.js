466e97f173b69bd140a8bcbeec5d911138d57e81
Shawn Rushefsky <shawn.rushefsky@gmail.com>
2022-01-10 12:29:43 -0500

    issue128

--- checks/valid-bash-substitution.js
+++ checks/valid-bash-substitution.js
@@ -2,9 +2,7 @@ require("../typedefs");
 const log = require("loglevel");
 
 // we expect these to match the regex, but they should not be flagged
-const EXCLUDED_VARIABLE_NAMES = [
-  'CMD',
-];
+const EXCLUDED_VARIABLE_NAMES = ["CMD", "ENTRYPOINT", "HTPASSWD"];
 const singleQuoteSubsitution = /export (?<variable>\w+)='.*\$({|)\w+(}|).*'/;
 
 /**
