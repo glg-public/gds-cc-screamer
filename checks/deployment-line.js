199ee234bf88d0a92afa33ec55f8b58beaba5ca0
Shawn Rushefsky <shawn.rushefsky@gmail.com>
2022-02-22 12:02:47 -0500

    can be 0 characters

--- checks/deployment-line.js
+++ checks/deployment-line.js
@@ -2,9 +2,9 @@ require("../typedefs");
 const log = require("loglevel");
 
 const dockerdeploy =
-  /^dockerdeploy (?<source>\w+)\/(?<org>[\w-]+)\/(?<repo>.+?)\/(?<branch>[^:]+):?(?<tag>[\w-]+)/;
+  /^dockerdeploy (?<source>\w+)\/(?<org>[\w-]+)\/(?<repo>.+?)\/(?<branch>[^:]+):?(?<tag>[\w-]*)/;
 const jobdeploy =
-  /^jobdeploy (?<source>\w+)\/(?<org>[\w-]+)\/(?<repo>.+?)\/(?<branch>[^:]+):?(?<tag>[\w-]+)/;
+  /^jobdeploy (?<source>\w+)\/(?<org>[\w-]+)\/(?<repo>.+?)\/(?<branch>[^:]+):?(?<tag>[\w-]*)/;
 const autodeploy =
   /^autodeploy\s+(git@github.com:|https:\/\/github\.com\/)(?<org>[\w-]+)\/(?<repo>[^#\.]+)(\.git|)#?(?<branch>.*)/;
 const validCharacters = /^[a-z][a-z0-9-]*$/;
